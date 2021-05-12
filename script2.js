c361621379cf7d11bc1896f67e5865b0c2927d0b
jacekpytka <83397945+jacekpytka@users.noreply.github.com>
2021-05-12 10:57:26 +0200

    plenty of changes

--- script2.js
+++ script2.js
@@ -1,19 +1,14 @@
-let euroElement = document.querySelector(".js-euro");
-let exchangeRate = document.querySelector(".js-exchangeRate");
-let exchangeResult = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
+{
+const euroElement = document.querySelector(".js-euro");
+const exchangeRate = document.querySelector(".js-exchangeRate");
+const exchangeResult = document.querySelector(".js-result");
+const formElement = document.querySelector(".js-form");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    let euro = euroElement.value;
-    let exchange = exchangeRate.value;
+    const euro = euroElement.value;
+    const exchange = exchangeRate.value;
     let result = euro * exchange;
-
-
     exchangeResult.innerText = result.toFixed(2);
-
-
-
-});
+})};
 
